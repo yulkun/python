83645ecfd8c1691681bb2ddfcca7c9e413d882a4
yulkun <green4652@naver.com>
2020-02-09 14:49:15 +0900

    study today

---
+++ 4. A나누기 B 프로그램.py
@@ -0,0 +1,4 @@
+a = input().split( )
+A = int(a[0])
+B = int(a[1])
+print(A/B)
---
+++ 8. 개.py
@@ -0,0 +1,6 @@
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )"""\ ')
+print('|"^"`    |')
+a = "||_/=\ \__|"
+print(a.replace(" ", ""))
