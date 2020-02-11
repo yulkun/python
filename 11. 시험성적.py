6a84e26690863a05b627b5b4486d97757c65b3a6
yulkun <green4652@naver.com>
2020-02-11 22:34:17 +0900

    Create 11. 시험성적.py
    시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.

---
+++ 11. 시험성적.py
@@ -0,0 +1,11 @@
+a = input()
+if 90 <= int(a) <= 100:
+    print("A")
+if 80 <= int(a) <= 89:
+    print("B")
+if 70 <= int(a) <= 79:
+    print("C")
+if 60 <= int(a) <= 69:
+    print("D")
+if int(a) < 60:
+    print("F")
